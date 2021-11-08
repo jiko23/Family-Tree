a75c3d34ce04d36b46ef6f2fc018f8a7a0ede550
JAYANTA CHOUDHURY <38100332+jiko23@users.noreply.github.com>
2021-11-08 10:23:37 +0530

    Holds numerical representation of family members.

---
+++ family.py
@@ -0,0 +1,8 @@
+"""
+	This dictionary holds the numerical representaions of the family members
+	for the ease of building and functioning of the family. If any new member 
+	is added to the family then the numerical representaion of the member will
+	be also present in this dictionary.
+"""
+
+_family_dict={0 : 'King_Shan', 1 : 'Queen_Anga', 2:'Chit', 3:'Ish', 4:'Vich', 5:'Aras', 6:'Satya', 7:'Amba', 8:'Lika', 9:'Chitra', 10:'Vyan', 11:'Dritha',12:'Jaya',13:'Tritha', 14:'Vritha',15:'Vila',16:'Chika',17:'Arit',18:'Jnki',19:'Ahit',20:'Satvy',21:'Asva',22:'Krpi',23:'Vyas',24:'Atya',25:'Yodhan',26:'Laki',27:'Lavnya',28:'Vasa',29:'Kriya',30:'Krithi'}
